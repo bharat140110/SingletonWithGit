83fd16695a231f329b01ace83e996770af88ddf6
ldrat <ldrat@Bharat>
2022-02-22 18:09:45 +0530

    First Commit..

---
+++ src/main/java/com/nw/singleton/SingletonWithGitApplication.java
@@ -0,0 +1,13 @@
+package com.nw.singleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingletonWithGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingletonWithGitApplication.class, args);
+	}
+
+}
