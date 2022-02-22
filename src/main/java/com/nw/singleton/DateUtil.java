73145ff425ba0a47ba740c98590df58e5dd72682
ldrat <ldrat@Bharat>
2022-02-22 23:21:57 +0530

    Added Singleton DateUtil class to BranchX

---
+++ src/main/java/com/nw/singleton/DateUtil.java
@@ -0,0 +1,31 @@
+package com.nw.singleton;
+
+import java.io.Serializable;
+
+public class DateUtil  implements Serializable, Cloneable {
+
+	private static volatile DateUtil instance;
+
+	private DateUtil() {
+		System.out.println("Constuctor called....");
+	}
+
+	public static DateUtil getObject() {
+		if (instance == null) {
+			synchronized (DateUtil.class) {
+				if (instance == null)
+					instance = new DateUtil();
+			}
+		}
+		return instance;
+	}
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException("Cloning of this obj is not supported");
+	}
+
+	public Object readResolve() {
+		return instance;
+	}
+}
